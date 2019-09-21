db7fe269b7d373fcc4efbaa5bf296216e3ec169b
gunydev <gunmissly@gmail.com>
2019-09-21 23:37:32 +0700

    Added data feed and post data to google sheet

---
+++ src/assets/mixins/data.js
@@ -0,0 +1,34 @@
+const countrys = [
+  'THAILAND',
+  'INDONESIA',
+  'MALAYSIA',
+  'VIETNAM',
+  'AUSTRALIA',
+  'CAMBODIA',
+  'INDIA',
+  'CHINA',
+  'ALL-STAR'
+]
+
+const teams = [
+  { country: 'THAILAND', name: 'KORAT B' },
+  { country: 'THAILAND', name: 'PHITSANULOK FA' },
+  { country: 'THAILAND', name: 'HO' },
+  { country: 'THAILAND', name: 'FOOD' },
+  { country: 'INDONESIA', name: 'ABS' },
+  { country: 'INDONESIA', name: 'COLAS RAIL INDONESIA' },
+  { country: 'MALAYSIA', name: 'KBC' },
+  { country: 'MALAYSIA', name: 'COLAS RAIL MALAYSIA' },
+  { country: 'VIETNAM', name: 'ADCO' },
+  { country: 'VIETNAM', name: 'COLAS RAIL VIETNAM' },
+  { country: 'AUSTRALIA', name: 'COLAS AUSTRALIA' },
+  { country: 'CAMBODIA', name: 'TIPCO ASPHALT CAMBODIA' },
+  { country: 'INDIA', name: 'HINCOL INDIA' },
+  { country: 'CHINA', name: 'TIPCO ASPHALT CHINA' },
+  { country: 'ALL-STAR', name: 'ASIA ALL-STAR TEAM' }
+]
+
+export default {
+  countrys,
+  teams
+}
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -16,21 +16,6 @@
             <v-text-field hide-details outlined single-line v-model="formData.name"></v-text-field>
           </v-col>
         </v-row>
-        <!-- team name -->
-        <v-row align="center" justify="center">
-          <v-col cols="4">
-            <div class="body-1 text-right">Team Name</div>
-          </v-col>
-          <v-col class="py-1" cols="7">
-            <v-autocomplete
-              hide-details
-              :items="teams"
-              outlined
-              single-line
-              v-model="formData.teamName"
-            ></v-autocomplete>
-          </v-col>
-        </v-row>
         <!-- country -->
         <v-row align="center" justify="center">
           <v-col cols="4">
@@ -43,6 +28,24 @@
               outlined
               single-line
               v-model="formData.country"
+            ></v-autocomplete>
+          </v-col>
+        </v-row>
+        <!-- team name -->
+        <v-row align="center" justify="center">
+          <v-col cols="4">
+            <div class="body-1 text-right">Team Name</div>
+          </v-col>
+          <v-col class="py-1" cols="7">
+            <v-autocomplete
+              hide-details
+              :items="filterTeam"
+              item-text="name"
+              item-value="name"
+              outlined
+              :readonly="formData.country ? false : true"
+              single-line
+              v-model="formData.teamName"
             ></v-autocomplete>
           </v-col>
         </v-row>
@@ -71,6 +74,7 @@
           class="title font-weight-bold btn-confirm"
           @click="doSubmit()"
           depressed
+          :loading="loading"
           min-height="20"
           width="100"
         >confirm</v-btn>
@@ -81,14 +85,15 @@
 </template>
 
 <script>
+import dataFeed from '../assets/mixins/data'
 export default {
   data () {
     return {
-      step: 1,
+      loading: false,
       formData: [],
-      teams: ['asd', 'asd2'],
-      countrys: ['asd', 'asd2'],
-      numbers: ['asd', 'asd2'],
+      teams: dataFeed.teams,
+      countrys: dataFeed.countrys,
+      numbers: [1, 2, 3, 4, 5, 6, 7],
       rules: `
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
       Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
@@ -108,14 +113,47 @@ export default {
         default:
           return 'Account created'
       }
+    },
+    filterTeam () {
+      return this.teams.filter(i => i.country === this.formData.country)
     }
   },
+  async created () {},
   methods: {
     async doSubmit () {
       if (!this.formData.confirmCheck) {
         alert('Please confirm register rules')
         return 1
       }
+      this.loading = !this.loading
+      const id = await this.getLastId()
+      const dateTime = await this.getDate()
+      const football = {
+        id: id + 1,
+        name: this.formData.name,
+        team: this.formData.teamName,
+        country: this.formData.country,
+        number: this.formData.number,
+        accept_date: dateTime
+      }
+      const payload = {
+        data: [football]
+      }
+      const rs = await this.axios.post(`https://sheetdb.io/api/v1/92u4gqslzu1bk`, payload)
+      if (rs.status === 201 && rs.data.created === 1) {
+        this.loading = !this.loading
+        this.$router.push({ name: 'info' })
+      }
+    },
+    async getLastId () {
+      const rs = await this.axios.get(`https://sheetdb.io/api/v1/92u4gqslzu1bk/count`)
+      return rs.data ? rs.data.rows : 0
+    },
+    getDate () {
+      var today = new Date()
+      var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+      var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+      return date + ' ' + time
     }
   }
 }
